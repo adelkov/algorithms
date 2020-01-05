85273e82e2e3d793453334bcd401b51b27ac647d
Adel <kovacsadel12@gmail.com>
2020-01-05 16:29:08 +0100

    remove console log

--- leetcode/largestSubArray.js
+++ leetcode/largestSubArray.js
@@ -8,7 +8,6 @@ const maxSubArray = function (arr) {
     let localMax = arr[0];
     let max = arr[0];
     for (let i = 1; i < arr.length; i++) {
-        console.log(arr[i] + localMax, arr[i]);
         if (arr[i] + localMax >= arr[i]) {
             localMax += arr[i];
         } else {
